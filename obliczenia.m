6a0fc67c53c403604abbba3f0b76a288f076db04
Joasia Klasik <asklas@op.pl>
2024-03-22 07:47:09 +0100

    thirt classes

--- obliczenia.m
+++ obliczenia.m
@@ -6,7 +6,7 @@ TwewN = 20;     %oC
 TgpN = 70;      %oC
 TzewN = -20;    %oC
 TgzN = 90;      %oC
-QgN = 10000;    % moc grzałki W
+QgN = 2000;    % moc grzałki W
 QtN = 0;        % moc "wyłączonej" grzałki W
 TwzN = 130;     %oC
 TwpN = 110;     %oC
@@ -14,10 +14,10 @@ TozN = TgzN;    %oC
 TopN = TgpN;    %oC
 
 
-Vw = 2*6*6;     % pojemnośc pokoju m^3
-Vg = 1*0.5*0.5; % pojemność grzałki m^3
-Vvo1 = 7.5;     % pojemność 1 połowy węzła ciepłowniczego m^3
-Vvo2 = 7.5;     % pojemność 2 połowy węzła ciepłowniczego m^3
+Vw = 10*6*2;   % pojemnośc pokoju m^3
+Vg = 1*0.5*0.25; % pojemność grzałki m^3
+Vvo1 = 1;     % pojemność 1 połowy węzła ciepłowniczego m^3
+Vvo2 = 1;     % pojemność 2 połowy węzła ciepłowniczego m^3
 
 Cpw = 1000;     % ciepło własiściwe powietrza J/(kg*K)
 Cmg = 4175;     % ciepło własiściwe wody J/(kg*K)
@@ -27,18 +27,19 @@ Rog = 960;      % gęstość wody kg/m^3
 %-----------------------
 % identyfikacja parametrów statycznych
 FmgN = QgN/(Cpw*(TgzN - TgpN));
-Kcg = QgN/(TgpN-TwewN);  % sprawdz czy to jest dobrze
-Kcw = QgN/(TwewN-TzewN);   % sprawdz czy to jest dobrze
+Kcg = QgN/(TgpN-TwewN);                         
+Kcw = QgN/(TwewN-TzewN);
 Kco = 2*QgN/(TwpN-TozN);
 FmwN = Kco*(TwpN - TozN)/(Cpw*(TwzN - TwpN));
 FmoN = Kco*(TwpN - TozN)/(Cpw*(TozN - TopN));
 
 %-----------------------
 % identyfikacja parametrów dynamicznych
-Cvw = Cpw*Row*Vw;  
-Cvg = Cmg*Rog*Vg; 
-Cvo1 = Cmg*Rog*Vvo1; 
-Cvo2 = Cmg*Rog*Vvo1; 
+Cvw = Cpw*Row*Vw*2;       % pojemność cieplna powietrza w pokoju
+                          % 2 - chcemy zwiekszyc znaczenie ścian
+Cvg = Cmg*Rog*Vg;         % pojemność cieplna grzejnika
+Cvo1 = Cvg;    
+Cvo2 = Cvg;    
 
 %-----------------------
 % sprawdzenie
@@ -50,46 +51,74 @@ assert(0 == (Cpw*FmgN*(TgzN-TgpN)-Kcg*(TgpN - TwewN)), "Rownania statyczne niero
 %==================== PUNKT PRACY =======================%
 % warunki początkowe
 Tzew0 = TzewN + 0;  % 0 - nominalne
-Tgz0 = TgzN + 0;    % 0 - nominalne
+Tgz10 = TgzN + 0;    % 0 - nominalne
+Tgz20 = TgzN + 0;    % 0 - nominalne
 Qg0 = QgN*1.0;      % 1.0 - nominalne
 Fmg0 = FmgN*1.0;    % 1.0 - nominalne
 Twz0 = TwzN + 0;    % 0 - nominalnie
 
+Fmg10 = Fmg0;
+Fmg20 = Fmg0;
+
+T0 = 10; % s opoznienie czasowe
+
 % stan równowagi        (dla nominalnych warubkowpoczatkowcyh Twew0 = TwewN i Tgp0 = TgpN)
-Twew0 = (Cpw*Fmg0*Kcg*Tgz0 + Kcw*(Kcg + Cpw*Fmg0)*Tzew0)/(Kcg*Kcw + Cpw*Fmg0*(Kcg + Kcw));
-Tgp0 = ((Kcg + Kcw)*Twew0 - Kcw*Tzew0)/(Kcg);
-    Top0 = Tgp0;        % PRZYBLIZENIE 3
+Twew10 = (Cpw*Fmg10 *Kcg*Tgz10 + Kcw*(Kcg + Cpw*Fmg10)*Tzew0)/(Kcg*Kcw + Cpw*Fmg10*(Kcg + Kcw));
+Twew20 = (Cpw*Fmg20 *Kcg*Tgz20 + Kcw*(Kcg + Cpw*Fmg20)*Tzew0)/(Kcg*Kcw + Cpw*Fmg20*(Kcg + Kcw));
+Tgp10 = ((Kcg + Kcw)*Twew10 - Kcw*Tzew0)/(Kcg);
+Tgp20 = ((Kcg + Kcw)*Twew20 - Kcw*Tzew0)/(Kcg);
+
+Top0 = TopN;        % PRZYBLIZENIE 3
 Toz0 = TozN; % Fmg0*Kco/(Kco*Fmg0 + Cpw*Fmg0*Fmg0 + Kco*Fmg0)*(Twz0 + (Cpw*Fmg0)*Top0/(Kco)); % JESLI JEST ZLE PRZYJMIN TozN
 Twp0 = TwpN; % (Cpw*Fmg0+Kco)*Toz0/Kco - (Cpw*Fmg0 + Kco)*Top0/Kco; % JEŚLI JEST ŹLE PRZYJMIJ TwpN
 
+SumT0 = (Tgp10*Fmg10 + Tgp20*Fmg20)/(Fmg10 + Fmg20);
 
 %==================== SYMULACJE =======================%
 %symulacja
-tmax = 200;  % czas symulacji
+tmax = 100000;  % czas symulacji
 
 %zaklocenia
-tsok = 0; % czas skoku
+tsok = 2000; % czas skoku
 dFmg = 0;
-dTgz = 0;
 dTzew = 0;
 dQt = 0;
-% dTop = 0;
 dTwz = 0;
-dFmw = 0;
-% dFmo = 0;
+dFmw = 0.1*FmgN;
 
 model='model_rownan';
 [t]=sim(model,tmax);    % t - wektor czasu
 %wykresy
-figure, plot(t, Twew, 'r'), grid on, title("Reakcja Twew");
-xlabel("t[s]"), ylabel("Twew[^{\circ}C]");
+figure, plot(t, Twew1, 'r'), grid on, title("Reakcja Twew1 na skok Fmg o 10%");
+xlabel("t[s]"), ylabel("Twew1[^{\circ}C]");
 
-figure, plot(t, Tgp1, 'r'), grid on, title("Reakcja Tgp1");
-xlabel("t[s]"), ylabel("Tgp1[^{\circ}C]");
+%==================== IDENTYFIKACJ =======================%
+k1 = (20.2553 - 20)/dFmw;
+Topu1 = 5820- tsok;
+Tczas1 = 25390-Topu1;
 
-figure, plot(t, Twp, 'r'), grid on, title("Reakcja Twp");
-xlabel("t[s]"), ylabel("Tgp[^{\circ}C]");
+k2 = (20.2553 - 20)/dFmw;
+Tczas2 = 21425-11955;
+Topu2 = 21425 - Tczas2;
 
-figure, plot(t, Toz, 'r'), grid on, title("Reakcja Toz");
-xlabel("t[s]"), ylabel("Tgp[^{\circ}C]");
+
+figure, plot(t, Twew1, 'r'), grid on, title("Reakcja Twew1 na skok Fmg o 10%");
+xlabel("t[s]"), ylabel("Twew1[^{\circ}C]");
+hold on;
+modelPrzeg='identyfikacja';
+[t]=sim(modelPrzeg,tmax);    % t - wektor czasu
+%wykresy
+plot(t, TwewId1, 'b');
+plot(t, TwewId2, 'g');
+legend('obiekt rzeczywisty','identyfikacja - pkt przegiecia','identyfikacja - metoda dwupkt');
+
+
+% figure, plot(t, Tgp1, 'r'), grid on, title("Reakcja Tgp1");
+% xlabel("t[s]"), ylabel("Tgp1[^{\circ}C]");
+% 
+% figure, plot(t, Twp, 'r'), grid on, title("Reakcja Twp");
+% xlabel("t[s]"), ylabel("Tgp[^{\circ}C]");
+% 
+% figure, plot(t, Toz, 'r'), grid on, title("Reakcja Toz");
+% xlabel("t[s]"), ylabel("Tgp[^{\circ}C]");
 
